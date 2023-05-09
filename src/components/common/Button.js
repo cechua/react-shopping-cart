9b928ae232f6c609e1d1f95a49ac02bff5450e51
cechua <chuacelc08@gmail.com>
2023-05-09 22:33:45 +0800

    Added home page

---
+++ src/components/common/Button.js
@@ -0,0 +1,22 @@
+import clsx from "clsx";
+import React from "react";
+import { Link } from "react-router-dom";
+const Button = ({
+  text,
+  onClick,
+  className = "",
+  isLink = false,
+  urlLink = "",
+}) => {
+  return isLink && urlLink ? (
+    <Link to={urlLink} className={clsx("button-base", className)}>
+      {text}
+    </Link>
+  ) : (
+    <button className={clsx("button-base", className)} onClick={onClick}>
+      {text}
+    </button>
+  );
+};
+
+export default Button;
--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -1,7 +1,27 @@
 import React from "react";
-
+import banner from "../../images/banner.jpg";
+import Button from "components/common/Button";
 const HomePage = () => {
-  return <div>Home page</div>;
+  const onShopHandler = () => {
+    alert("test");
+  };
+  return (
+    <div className="home-page">
+      <div className="image-bg">
+        <img src={banner} alt="computer" className="image" />
+        <div className="banner-text">
+          <h6>Build your own PC today!</h6>
+          <Button
+            onClick={onShopHandler}
+            text="Shop Now"
+            className="link-button"
+            isLink={true}
+            urlLink="/shop"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
