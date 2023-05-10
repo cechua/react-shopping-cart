23b46825f541e41c514b7f84b999f5fb777833e2
cechua <chuacelc08@gmail.com>
2023-05-10 21:48:06 +0800

    Add shop menu and display multiple items using fake items

--- src/components/shopPage/ShopPage.js
+++ src/components/shopPage/ShopPage.js
@@ -10,22 +10,146 @@ const ShopPage = () => {
         "https://oyster.ignimgs.com/mediawiki/apis.ign.com/fire-emblem-nintendo-switch/6/60/Edelgard_Profile.jpg?width=640",
       itemName: "CPU TEST",
       itemPrice: "$123.00",
+      category: "Processor",
+      productId: "cpu_1",
+    },
+    {
+      imageSrc:
+        "https://oyster.ignimgs.com/mediawiki/apis.ign.com/fire-emblem-nintendo-switch/9/90/Dimitri_Profile.jpg?width=640",
+      imageSrcHover:
+        "https://oyster.ignimgs.com/mediawiki/apis.ign.com/fire-emblem-nintendo-switch/6/60/Edelgard_Profile.jpg?width=640",
+      itemName: "CPU TEST",
+      itemPrice: "$123.00",
+      category: "Processor",
+      productId: "cpu_1",
+    },
+    {
+      imageSrc:
+        "https://oyster.ignimgs.com/mediawiki/apis.ign.com/fire-emblem-nintendo-switch/9/90/Dimitri_Profile.jpg?width=640",
+      imageSrcHover:
+        "https://oyster.ignimgs.com/mediawiki/apis.ign.com/fire-emblem-nintendo-switch/6/60/Edelgard_Profile.jpg?width=640",
+      itemName: "CPU TEST",
+      itemPrice: "$123.00",
+      category: "Processor",
+      productId: "cpu_1",
+    },
+    {
+      imageSrc:
+        "https://oyster.ignimgs.com/mediawiki/apis.ign.com/fire-emblem-nintendo-switch/9/90/Dimitri_Profile.jpg?width=640",
+      imageSrcHover:
+        "https://oyster.ignimgs.com/mediawiki/apis.ign.com/fire-emblem-nintendo-switch/6/60/Edelgard_Profile.jpg?width=640",
+      itemName: "CPU TEST",
+      itemPrice: "$123.00",
+      category: "Processor",
+      productId: "cpu_1",
+    },
+    {
+      imageSrc:
+        "https://oyster.ignimgs.com/mediawiki/apis.ign.com/fire-emblem-nintendo-switch/9/90/Dimitri_Profile.jpg?width=640",
+      imageSrcHover:
+        "https://oyster.ignimgs.com/mediawiki/apis.ign.com/fire-emblem-nintendo-switch/6/60/Edelgard_Profile.jpg?width=640",
+      itemName: "CPU TEST",
+      itemPrice: "$123.00",
+      category: "Processor",
+      productId: "cpu_1",
+    },
+    {
+      imageSrc:
+        "https://oyster.ignimgs.com/mediawiki/apis.ign.com/fire-emblem-nintendo-switch/9/90/Dimitri_Profile.jpg?width=640",
+      imageSrcHover:
+        "https://oyster.ignimgs.com/mediawiki/apis.ign.com/fire-emblem-nintendo-switch/6/60/Edelgard_Profile.jpg?width=640",
+      itemName: "CPU TEST",
+      itemPrice: "$123.00",
+      category: "Processor",
+      productId: "cpu_1",
+    },
+    {
+      imageSrc:
+        "https://oyster.ignimgs.com/mediawiki/apis.ign.com/fire-emblem-nintendo-switch/9/90/Dimitri_Profile.jpg?width=640",
+      imageSrcHover:
+        "https://oyster.ignimgs.com/mediawiki/apis.ign.com/fire-emblem-nintendo-switch/6/60/Edelgard_Profile.jpg?width=640",
+      itemName: "CPU TEST",
+      itemPrice: "$123.00",
+      category: "Processor",
+      productId: "cpu_1",
+    },
+    {
+      imageSrc:
+        "https://oyster.ignimgs.com/mediawiki/apis.ign.com/fire-emblem-nintendo-switch/9/90/Dimitri_Profile.jpg?width=640",
+      imageSrcHover:
+        "https://oyster.ignimgs.com/mediawiki/apis.ign.com/fire-emblem-nintendo-switch/6/60/Edelgard_Profile.jpg?width=640",
+      itemName: "CPU TEST",
+      itemPrice: "$123.00",
+      category: "Processor",
+      productId: "cpu_1",
+    },
+    {
+      imageSrc:
+        "https://oyster.ignimgs.com/mediawiki/apis.ign.com/fire-emblem-nintendo-switch/9/90/Dimitri_Profile.jpg?width=640",
+      imageSrcHover:
+        "https://oyster.ignimgs.com/mediawiki/apis.ign.com/fire-emblem-nintendo-switch/6/60/Edelgard_Profile.jpg?width=640",
+      itemName: "CPU TEST",
+      itemPrice: "$123.00",
+      category: "Processor",
+      productId: "cpu_1",
+    },
+    {
+      imageSrc:
+        "https://oyster.ignimgs.com/mediawiki/apis.ign.com/fire-emblem-nintendo-switch/9/90/Dimitri_Profile.jpg?width=640",
+      imageSrcHover:
+        "https://oyster.ignimgs.com/mediawiki/apis.ign.com/fire-emblem-nintendo-switch/6/60/Edelgard_Profile.jpg?width=640",
+      itemName: "CPU TEST",
+      itemPrice: "$123.00",
+      category: "Processor",
+      productId: "cpu_1",
+    },
+    {
+      imageSrc:
+        "https://oyster.ignimgs.com/mediawiki/apis.ign.com/fire-emblem-nintendo-switch/9/90/Dimitri_Profile.jpg?width=640",
+      imageSrcHover:
+        "https://oyster.ignimgs.com/mediawiki/apis.ign.com/fire-emblem-nintendo-switch/6/60/Edelgard_Profile.jpg?width=640",
+      itemName: "CPU TEST",
+      itemPrice: "$123.00",
+      category: "Processor",
+      productId: "cpu_1",
+    },
+    {
+      imageSrc:
+        "https://oyster.ignimgs.com/mediawiki/apis.ign.com/fire-emblem-nintendo-switch/9/90/Dimitri_Profile.jpg?width=640",
+      imageSrcHover:
+        "https://oyster.ignimgs.com/mediawiki/apis.ign.com/fire-emblem-nintendo-switch/6/60/Edelgard_Profile.jpg?width=640",
+      itemName: "CPU TEST",
+      itemPrice: "$123.00",
+      category: "Processor",
+      productId: "cpu_1",
     },
   ];
   const onClickHandler = () => {
     alert("test");
   };
   return (
-    <div className="items-container">
-      {items.map((item) => (
-        <Card
-          imageSrc={item.imageSrc}
-          imageSrcHover={item.imageSrcHover}
-          cardText={item.itemName}
-          cardSecondaryText={item.itemPrice}
-          onClickHandler={onClickHandler}
-        />
-      ))}
+    <div className="shop-container">
+      <div className="category-container">
+        <ul>
+          <li>Motherboards</li>
+          <li>Processors</li>
+          <li>Graphics Card</li>
+          <li>RAM</li>
+          <li>Fans</li>
+          <li>Computer Cases</li>
+        </ul>
+      </div>
+      <div className="items-container">
+        {items.map((item) => (
+          <Card
+            imageSrc={item.imageSrc}
+            imageSrcHover={item.imageSrcHover}
+            cardText={item.itemName}
+            cardSecondaryText={item.itemPrice}
+            onClickHandler={onClickHandler}
+          />
+        ))}
+      </div>
     </div>
   );
 };
